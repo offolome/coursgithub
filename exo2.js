3785ef8511aba15e72705ffebab0685fcbf3e150
unknown <offado>
2024-09-20 21:29:21 +0000

    Exercices

---
+++ exo2.js
@@ -0,0 +1,12 @@
+// Exercice: Opposite number
+
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+
+function oppositeNumber(number) {
+    return -number;
+}
+
+console.log(oppositeNumber(1));
+console.log(oppositeNumber(14));
+console.log(oppositeNumber(-34));
